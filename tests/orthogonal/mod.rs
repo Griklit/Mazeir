edfe4f05ff242b27cd831b7871b88b9fc786510d
Sequence[7] <sequence7@scalt.sct>
2023-08-25 14:45:52 +0800

    test

--- tests/orthogonal/mod.rs
+++ tests/orthogonal/mod.rs
@@ -8,16 +8,25 @@ use mazeir::output::Draw;
 use crate::assert_func::*;
 
 
-fn get_path(arithmetic: &str, width: usize, height: usize) -> (PathBuf, PathBuf) {
-    let snapshot_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(format!("tests/orthogonal/snapshot/{}_{}x{}.png", arithmetic, width, height));
-    let output_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(format!("tests/orthogonal/output/{}_{}x{}.png", arithmetic, width, height));
+fn get_path(file_name: &str) -> (PathBuf, PathBuf) {
+    let snapshot_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(format!("tests/orthogonal/snapshot/{}.png", file_name));
+    let output_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(format!("tests/orthogonal/output/{}.png", file_name));
     (snapshot_path, output_path)
+}
+
+#[test]
+fn depth_first_default() {
+    let mut map = Orthogonal::default();
+    let (snapshot_path, output_path) = get_path("depth-first_default");
+    map.depth_first();
+    map.draw(fs::File::create(&output_path).unwrap()).unwrap();
+    assert_files_eq(&output_path, &snapshot_path);
 }
 
 #[test]
 fn depth_first_0x0() {
     let mut map = Orthogonal::new(0, 0);
-    let (snapshot_path, output_path) = get_path("depth-first", 0, 0);
+    let (snapshot_path, output_path) = get_path("depth-first_0x0");
     map.depth_first_with_str_seed("0x0");
     map.draw(fs::File::create(&output_path).unwrap()).unwrap();
     assert_files_eq(&output_path, &snapshot_path);
@@ -26,7 +35,7 @@ fn depth_first_0x0() {
 #[test]
 fn depth_first_2x3() {
     let mut map = Orthogonal::new(2, 3);
-    let (snapshot_path, output_path) = get_path("depth-first", 2, 3);
+    let (snapshot_path, output_path) = get_path("depth-first_2x3");
     map.depth_first_with_str_seed("2x3");
     map.draw(fs::File::create(&output_path).unwrap()).unwrap();
     assert_files_eq(&output_path, &snapshot_path);
@@ -35,7 +44,7 @@ fn depth_first_2x3() {
 #[test]
 fn depth_first_1076x593() {
     let mut map = Orthogonal::new(1076, 593);
-    let (snapshot_path, output_path) = get_path("depth-first", 1076, 593);
+    let (snapshot_path, output_path) = get_path("depth-first_1076x593");
     map.depth_first_with_str_seed("1076x593");
     map.draw(fs::File::create(&output_path).unwrap()).unwrap();
     assert_files_eq(&output_path, &snapshot_path);
